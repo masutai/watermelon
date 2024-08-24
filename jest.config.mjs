5a0ad35ab57772c38a3bc27976c719ba34f69c01
key-books <eijikagimoto@gmail.com>
2024-08-25 00:24:00 +0900

    style: lintの適用

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,17 +1,17 @@
-import nextJest from "next/jest.js";
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  dir: "./src",
+  dir: './src',
 });
 
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    "^@/components/(.*)$": "<rootDir>/src/components/$1",
-    "^@/app/(.*)$": "<rootDir>/src/app/$1",
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/app/(.*)$': '<rootDir>/src/app/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    dirs: ["pages", "components", "lib", "utils", "styles"],
+    dirs: ['pages', 'components', 'lib', 'utils', 'styles'],
   },
 };
 
