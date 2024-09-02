c1d607fa90cfa3c750dc6e4473bbdd8867e77250
key-books <eijikagimoto@gmail.com>
2024-09-02 21:36:16 +0900

    update: ヘッダーコンポーネントをshadcn/uiのヘッダーらしくした

--- src/components/layouts/Header/header.tsx
+++ src/components/layouts/Header/header.tsx
@@ -1,11 +1,14 @@
-import { ModeToggle } from "@/components/mode-toggle";
+import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
+import SubNav from "./sub-nav";
 
 export default function Header() {
   return (
     // ヘッダーの位置がわかるように一時的に背景を設定
     <header className="flex fixed w-full items-center justify-between bg-gray-200 dark:bg-gray-800">
-      <h1>Header</h1>
-      <ModeToggle />
+      <MainNav />
+      <MobileNav />
+      <SubNav />
     </header>
-  )
+  );
 }
