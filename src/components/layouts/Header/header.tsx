f9f85ece31129f4c1511903015c0334846b68e82
nyk9 <136601792+nyk9@users.noreply.github.com>
2024-09-07 19:12:16 +0900

    Revert "単語の型を定義"

--- src/components/layouts/Header/header.tsx
+++ src/components/layouts/Header/header.tsx
@@ -1,14 +1,11 @@
-import { MainNav } from "./main-nav";
-import { MobileNav } from "./mobile-nav";
-import SubNav from "./sub-nav";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Header() {
   return (
     // ヘッダーの位置がわかるように一時的に背景を設定
     <header className="flex fixed w-full items-center justify-between bg-gray-200 dark:bg-gray-800">
-      <MainNav />
-      <MobileNav />
-      <SubNav />
+      <h1>Header</h1>
+      <ModeToggle />
     </header>
-  );
+  )
 }
