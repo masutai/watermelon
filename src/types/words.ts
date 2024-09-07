d470cb29e70ee7607b238b138087f61bd84c9f1f
nyk9 <eijikagimoto@gamil.com>
2024-09-07 19:10:25 +0900

    feat: 単語の型を追加

---
+++ src/types/words.ts
@@ -0,0 +1,22 @@
+export interface Word {
+  name: string;
+  description_eng: string;
+  type:
+    | "Noun"
+    | "Verb"
+    | "Adjective"
+    | "Adverb"
+    | "Preposition"
+    | "Conjunction"
+    | "Interjection"
+    | "Other";
+  description_ja?: string;
+  region?: "World" | "American" | "British" | "Australian" | "Other";
+  american_accent?: string;
+  british_accent?: string;
+  australian_accent?: string;
+  american_spelling?: string;
+  british_spelling?: string;
+  australian_spelling?: string;
+  phenetic_symbol?: string;
+}
