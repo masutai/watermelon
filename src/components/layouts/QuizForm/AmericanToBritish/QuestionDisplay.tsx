0d2f0c0a53cc13b9c3c6f6e00da61dc5e3512b71
key-books <eijikagimoto@gmail.com>
2024-09-11 22:01:43 +0900

    remove: クイズ共通のコンポーネントに再設計

--- src/components/layouts/QuizForm/AmericanToBritish/QuestionDisplay.tsx
+++
@@ -1,7 +0,0 @@
-import { AmericanToBritish } from "@/types/amerian2british";
-
-export default function QuestionDisplay({ question }: { question: AmericanToBritish | null }) {
-  if (!question) return <p>読み込み中...</p>;
-
-  return <p>{question.american_spelling}をイギリス英語に直してください</p>;
-}
--- src/components/layouts/QuizForm/AmericanToBritish/ResultDisplay.tsx
+++
@@ -1,5 +0,0 @@
-export default function ResultDisplay({ isCorrect }: { isCorrect: boolean | null }) {
-  if (isCorrect === null) return null;
-
-  return <p>{isCorrect ? "正解！次の問題に進みます。" : "不正解。もう一度試してください。"}</p>;
-}
