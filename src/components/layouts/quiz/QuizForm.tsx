b570e0495272d70ba869a827cd2897ea68fcc5f7
key-books <eijikagimoto@gmail.com>
2024-09-13 14:25:38 +0900

    feat: 選択肢をランダムに並び替える機能の実装
    出題するごとに増える質問回数の変数を追加した

--- src/components/layouts/quiz/QuizForm.tsx
+++ src/components/layouts/quiz/QuizForm.tsx
@@ -2,13 +2,14 @@
 
 import { useQuizLogic } from "@/hooks/useQuizLogic";
 import { QuizData, isBritishQuizItem, isEngineerQuizItem } from "@/types/quiz";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BritishQuizForm from "./BritishQuizForm";
 import EngineerQuizForm from "./EngineerQuizForm";
 import ResultDisplay from "./common/ResultDisplay";
 
 export default function QuizForm({ quizData }: { quizData: QuizData }) {
   const { currentQuestion, updateQuestion, isCorrect, setIsCorrect } = useQuizLogic(quizData.items);
+  const [questionCount, setQuestionCount] = useState(0);
 
   useEffect(() => {
     updateQuestion();
@@ -20,6 +21,7 @@ export default function QuizForm({ quizData }: { quizData: QuizData }) {
       setTimeout(() => {
         updateQuestion();
         setIsCorrect(null);
+        setQuestionCount((prev) => prev + 1);
       }, 1500);
     }
   };
@@ -30,7 +32,13 @@ export default function QuizForm({ quizData }: { quizData: QuizData }) {
     if (isBritishQuizItem(currentQuestion)) {
       return <BritishQuizForm question={currentQuestion} onSubmit={handleSubmit} />;
     } else if (isEngineerQuizItem(currentQuestion)) {
-      return <EngineerQuizForm question={currentQuestion} onSubmit={handleSubmit} />;
+      return (
+        <EngineerQuizForm
+          question={currentQuestion}
+          onSubmit={handleSubmit}
+          questionCount={questionCount}
+        />
+      );
     }
   };
 
