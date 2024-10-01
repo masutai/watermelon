0e1b1d31b5b5e2ffbf86d807ae03f5e36d971987
nyk9 <eijikagimoto@gamil.com>
2024-10-01 15:39:48 +0900

    update: テーマ・ヘッダーの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import Header from "@/components/layouts/Header/header";
-import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,10 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <Header />
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
