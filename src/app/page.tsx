e7cd8892f4ab91e49d94ea38fe574d1d8d3c3601
key-books <eijikagimoto@gmail.com>
2024-10-02 22:57:38 +0900

    feat: メイン画面にゲームコンポーネントを描写

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Game from "@/components/layouts/Game/Game";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="">
+      <Game />
     </main>
   );
 }
