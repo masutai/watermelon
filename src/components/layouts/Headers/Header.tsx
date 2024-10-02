560938f280f2526931e236d2c41cb02c565dc373
key-books <eijikagimoto@gmail.com>
2024-10-02 22:54:19 +0900

    style: prettierで整えた

--- src/components/layouts/Headers/Header.tsx
+++ src/components/layouts/Headers/Header.tsx
@@ -2,10 +2,10 @@ import MainNav from "./Main-nav";
 import SubNav from "./Sub-nav";
 
 export default function Header() {
-    return (
-        <header>
-            <MainNav />
-            <SubNav />
-        </header>
-    )
+  return (
+    <header>
+      <MainNav />
+      <SubNav />
+    </header>
+  );
 }
