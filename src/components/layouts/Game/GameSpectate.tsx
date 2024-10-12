0b4206f2f97a22de455ec3ac82d794845205ce30
nyk9 <eijikagimoto@gamil.com>
2024-10-12 16:45:36 +0900

    add: 観戦者用のコンポーネントを作成
    pusherからデータを取得し、GameViewコンポーネントの引数に渡す

---
+++ src/components/layouts/Game/GameSpectate.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { GameModel } from "@/lib/game/gameModel";
+import { pusherClient } from "@/lib/pusher/client";
+import { useEffect, useState } from "react";
+import GameView from "./GameView";
+
+export default function GameSpectate() {
+  const [gameModel, setGameModel] = useState<GameModel>(new GameModel());
+  useEffect(() => {
+    const channel = pusherClient.subscribe("private-game").bind("evt::game", (data: GameModel) => {
+      console.log("received_from_pusher", data);
+      setGameModel(data);
+    });
+    return () => {
+      channel.unbind();
+    };
+  });
+  return (
+    <div>
+      <GameView
+        characterPosition={gameModel.characterPosition}
+        watermelonPosition={gameModel.watermelonPosition}
+        hitPosition={gameModel.hitPosition}
+      />
+    </div>
+  );
+}
