5128f858c1b465843144a1f1b94864aa890b7378
nyk9 <eijikagimoto@gamil.com>
2024-10-17 13:10:19 +0900

    Refactor page component to add new card templates and use the one-time code hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import GameController from "@/components/layouts/Game/GameController";
+"use client";
+
+import CardTemplate from "@/components/layouts/Card/CardTemplate";
+import OTCButton from "@/components/layouts/Card/OTC";
+import { useOneTimeCode } from "@/hooks/useOneTimeCode";
 
 export default function Home() {
+  const code = useOneTimeCode();
   return (
     <main className="">
-      <GameController />
+      <CardTemplate
+        title="ペアリング"
+        content="ペアリングコードを生成し、プレイページへ遷移する"
+        ButtonComponent={OTCButton}
+      />
+      <CardTemplate title="プレイする" content="" link="/play" buttonText="Click here" />
+      <CardTemplate title="プレイする" content="" link="/play" buttonText="Click here" />
+      <CardTemplate title="応援する" content="" link="/view" buttonText="Click here" />
     </main>
   );
 }
