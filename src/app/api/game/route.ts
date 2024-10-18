d64f6fda2fd1b25723ab5735b692f999b05076ab
key-books <eijikagimoto@gmail.com>
2024-10-18 23:40:47 +0900

    fix: ペアリングコードで観戦画面を描写するための改善とハイドレーションエラーの解消

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -5,18 +5,21 @@ const pusherServer = getPusherInstance();
 export const dynamic = "force-dynamic";
 
 export async function POST(req: Request, res: Response) {
-  const { characterPosition, watermelonPosition, hitPosition, isCollision } = await req.json();
+  const { characterPosition, watermelonPosition, hitPosition, isCollision, pairingCode } =
+    await req.json();
   try {
-    await pusherServer.trigger("private-game", "evt::game", {
+    await pusherServer.trigger(`private-game-${pairingCode}`, "evt::game", {
       characterPosition,
       watermelonPosition,
       hitPosition,
       isCollision
     } as GameModel);
 
-    return Response.json({ message: "Sockets tested" }, { status: 200 });
+    return Response.json(
+      { gameModel: { characterPosition, watermelonPosition, hitPosition, isCollision } },
+      { status: 200 }
+    );
   } catch (error) {
-    console.log(error);
     return Response.json({ message: "Failed to send message", error: error }, { status: 500 });
   }
 }
