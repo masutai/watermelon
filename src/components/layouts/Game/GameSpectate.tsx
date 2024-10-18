913f1091b36cc81d2fe5b021d34265604e411b4a
nyk9 <eijikagimoto@gamil.com>
2024-10-18 22:23:57 +0900

    refactor: ペアリングコードでプライベートチャンネルに飛べるように変更

--- src/components/layouts/Game/GameSpectate.tsx
+++ src/components/layouts/Game/GameSpectate.tsx
@@ -5,13 +5,15 @@ import { pusherClient } from "@/lib/pusher/client";
 import { useEffect, useState } from "react";
 import GameView from "./GameView";
 
-export default function GameSpectate() {
+export default function GameSpectate({ pairingCode }: { pairingCode: string }) {
   const [gameModel, setGameModel] = useState<GameModel>(new GameModel());
   useEffect(() => {
-    const channel = pusherClient.subscribe("private-game").bind("evt::game", (data: GameModel) => {
-      console.log("received_from_pusher", data);
-      setGameModel(data);
-    });
+    const channel = pusherClient
+      .subscribe(`private-game:${pairingCode}`)
+      .bind("evt::game", (data: GameModel) => {
+        console.log("received_from_pusher", data);
+        setGameModel(data);
+      });
     return () => {
       channel.unbind();
     };
