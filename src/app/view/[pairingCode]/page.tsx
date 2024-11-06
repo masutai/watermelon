dc229c7c5c1f40d9a4982d7105fd9d8f3375b5ad
nyk9 <eijikagimoto@gmail.com>
2024-11-06 11:36:44 +0900

    refactor: ダイナミックルートを15版に修正

--- src/app/view/[pairingCode]/page.tsx
+++ src/app/view/[pairingCode]/page.tsx
@@ -1,5 +1,6 @@
 import GameSpectate from "@/components/layouts/Game/GameSpectate";
 
-export default function View({ params }: { params: { pairingCode: string } }) {
-  return <GameSpectate pairingCode={params.pairingCode} />;
+export default async function View({ params }: { params: Promise<{ pairingCode: string }> }) {
+  const pairingCode = (await params).pairingCode;
+  return <GameSpectate pairingCode={pairingCode} />;
 }
