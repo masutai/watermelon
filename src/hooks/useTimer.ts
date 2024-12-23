dbcdd5985a95197612b9921690182f55b3047b3f
nyk9 <eijikagimoto@gmail.com>
2024-12-23 16:20:55 +0900

    feat: ゲームに制限時間機能を追加

---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useTimer() {
+  const gameTimer = 300;
+  const [timer, setTimer] = useState(gameTimer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prev) => {
+        if (prev <= 1) {
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const minutes = Math.floor(timer / 60);
+  const seconds = timer % 60;
+
+  const formattedTime = `${minutes}分${seconds}秒`;
+
+  return formattedTime;
+}
